04c2bef0ad3317c7ae4d77eddeb96b61a682c314
Beny Hasanudin <bhasanudin@gmail.com>
2024-10-23 11:05:01 +0700

    String template examples.

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 #!/bin/env python
 import string
 def main():
-    values = {'var': ''}
+    values = {'var': 'foo'}
+
+    t = string.Template("""
+                        Variable : $var
+                        Escape   : $$
+                        Variable in text : ${var}iable
+                        """)
+
+    print('TEMPLATE:', t.substitute(values))
+
+    s = """
+    Variable : %(var)s
+    Escape : %%
+    Variable in text: %(var)siable
+    """
+
+    print('INTERPOLATION:', s % values)
+
+    s = """
+    Variable : {var}
+    Escape : {{}}
+    Variable in text: {var}iable
+    """
+    
+    print('FORMAT:', s.format(**values))
+
+
 if __name__ == "__main__":
     main()
