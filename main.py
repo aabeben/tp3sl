4df1109093997b08e9b98c10410af050e35fe909
Beny Hasanudin <bhasanudin@gmail.com>
2024-10-25 02:38:19 +0700

    Indenting block.

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from textwrap_example import sample_text
 
 def main():
     dedented_text = textwrap.dedent(sample_text)
-    for width in [45, 60]:
-        print('{} Columns:\n'.format(width))
-        print(textwrap.fill(dedented_text, width=width))
-        print()
+    wrapped = textwrap.fill(dedented_text, width=50)
+    wrapped += '\n\nSecond paragraph after a blank line.'
+    final = textwrap.indent(wrapped, '> ')
+    print('Quoted block:\n')
+    print(final)
 
 if __name__ == "__main__":
     main()
