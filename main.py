6de36ec26e6a509512ed3a5de08914366a3ee809
Beny Hasanudin <bhasanudin@gmail.com>
2024-10-25 02:55:26 +0700

    Truncating long text.

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ import textwrap
 from textwrap_example import sample_text
 
 def main():
-    dedented_text = textwrap.dedent(sample_text).strip()
-    print(textwrap.fill(dedented_text, initial_indent='', subsequent_indent=' ' * 4, width=50))
+    dedented_text = textwrap.dedent(sample_text)
+    original = textwrap.fill(dedented_text, width=50)
+
+    print('Original:\n')
+    print(original)
+
+    shortened = textwrap.shorten(original, 100)
+    shortened_wrapped = textwrap.fill(shortened, width=50)
+
+    print('Shortened:\n')
+    print(shortened_wrapped)
 
 if __name__ == "__main__":
     main()
