488f0483ffb9cfde0ba0196716607792034e4962
Beny Hasanudin <bhasanudin@gmail.com>
2024-10-25 02:46:31 +0700

    predicate is used for prefixing lines if it returns true.

--- main.py
+++ main.py
@@ -2,12 +2,15 @@
 import textwrap
 from textwrap_example import sample_text
 
+def should_indent(line):
+    print('Indent {!r}?'.format(line))
+    return len(line.strip()) % 2 == 0
+
 def main():
     dedented_text = textwrap.dedent(sample_text)
     wrapped = textwrap.fill(dedented_text, width=50)
-    wrapped += '\n\nSecond paragraph after a blank line.'
-    final = textwrap.indent(wrapped, '> ')
-    print('Quoted block:\n')
+    final = textwrap.indent(wrapped, 'EVEN ', predicate=should_indent)
+    print('\nQuoted block:\n')
     print(final)
 
 if __name__ == "__main__":
