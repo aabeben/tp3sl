8259c7b33391e91dc990edb0d037be6a19e31960
Beny Hasanudin <bhasanudin@gmail.com>
2024-10-25 02:13:43 +0700

    Create a new type of template.

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 #!/bin/env python
+import re
 import string
 
+class MyTemplate(string.Template):
+    delimiter='{{'
+    pattern=r'''
+    \{\{(?:
+    (?P<escaped>\{\{)|
+    (?P<named>[_a-z][_a-z0-9]*)\}\}|
+    (?P<braced>[_a-z][_a-z0-9]*)\}\}|
+    (?P<invalid>)
+    )
+    '''
+
 def main():
-    t = string.Template('$var')
-    print(t.pattern.pattern)
+    t = MyTemplate('''
+                   {{{{
+                   {{var}}
+                   ''')
+    print('MATCHES:', t.pattern.findall(t.template))
+    print('SUBSTITUTE:', t.safe_substitute(var='replacement'))
 
 if __name__ == "__main__":
     main()
