4c061f7ce2beb0440c38bfce60a58cc9ed8a096b
Beny Hasanudin <bhasanudin@gmail.com>
2024-10-25 02:52:10 +0700

    Hanging indent.

--- main.py
+++ main.py
@@ -2,16 +2,9 @@
 import textwrap
 from textwrap_example import sample_text
 
-def should_indent(line):
-    print('Indent {!r}?'.format(line))
-    return len(line.strip()) % 2 == 0
-
 def main():
-    dedented_text = textwrap.dedent(sample_text)
-    wrapped = textwrap.fill(dedented_text, width=50)
-    final = textwrap.indent(wrapped, 'EVEN ', predicate=should_indent)
-    print('\nQuoted block:\n')
-    print(final)
+    dedented_text = textwrap.dedent(sample_text).strip()
+    print(textwrap.fill(dedented_text, initial_indent='', subsequent_indent=' ' * 4, width=50))
 
 if __name__ == "__main__":
     main()
