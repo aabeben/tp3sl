a0510763893c1d5fbb45d75f7a755d7826bb63f5
Beny Hasanudin <bhasanudin@gmail.com>
2024-10-25 02:34:57 +0700

    Combining dedent and fill.

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from textwrap_example import sample_text
 
 def main():
     dedented_text = textwrap.dedent(sample_text)
-    print('Dedented:')
-    print(dedented_text)
-
+    for width in [45, 60]:
+        print('{} Columns:\n'.format(width))
+        print(textwrap.fill(dedented_text, width=width))
+        print()
 
 if __name__ == "__main__":
     main()
