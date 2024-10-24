6f4c0f290e07b72606ae515b7d37daa3607aa6f0
Beny Hasanudin <bhasanudin@gmail.com>
2024-10-24 09:02:42 +0700

    Using string.Template.

--- main.py
+++ main.py
@@ -1,32 +1,13 @@
 #!/bin/env python
 import string
+
+values = {'var': 'foo'}
+t = string.Template("$var is here but $missing is not provided")
 def main():
-    values = {'var': 'foo'}
-
-    t = string.Template("""
-                        Variable : $var
-                        Escape   : $$
-                        Variable in text : ${var}iable
-                        """)
-
-    print('TEMPLATE:', t.substitute(values))
-
-    s = """
-    Variable : %(var)s
-    Escape : %%
-    Variable in text: %(var)siable
-    """
-
-    print('INTERPOLATION:', s % values)
-
-    s = """
-    Variable : {var}
-    Escape : {{}}
-    Variable in text: {var}iable
-    """
-    
-    print('FORMAT:', s.format(**values))
-
-
+    try:
+        print('substitute()       :', t.substitute(values))
+    except KeyError as err:
+        print('ERROR:', str(err))
+print('safe_substitute():', t.safe_substitute(values))
 if __name__ == "__main__":
     main()
