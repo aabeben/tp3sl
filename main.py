af1d5b98a0cd4e68a9a9c3ac4db0b56e1dc93638
Beny Hasanudin <bhasanudin@gmail.com>
2024-10-25 00:07:25 +0700

    String template default pattern.

--- main.py
+++ main.py
@@ -2,22 +2,8 @@
 import string
 
 def main():
-    class MyTemplate(string.Template):
-        delimiter='%'
-        idpattern='[a-z]+_[a-z]+'
-
-    template_text='''
-        Delimiter: %%
-        Replaced: %with_underscore
-        Ignored: %notunderscore
-    '''
-    d={
-        'with_underscore':'replaced',
-        'notunderscore': 'not replaced'
-    }
-    t=MyTemplate(template_text)
-    print('Modified ID pattern:')
-    print(t.safe_substitute(d))
+    t = string.Template('$var')
+    print(t.pattern.pattern)
 
 if __name__ == "__main__":
     main()
