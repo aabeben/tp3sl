c5cb55b09f1ab4dff2ce0e06e1988799515ecfa0
Beny Hasanudin <bhasanudin@gmail.com>
2024-10-29 08:36:57 +0700

    Finding patterns in text.

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 #!/bin/env python
-import textwrap
-from textwrap_example import sample_text
+import re
 
 def main():
-    dedented_text = textwrap.dedent(sample_text)
-    original = textwrap.fill(dedented_text, width=50)
+    pattern = 'this'
+    text = 'Does this text match the pattern?'
 
-    print('Original:\n')
-    print(original)
+    match = re.search(pattern, text)
 
-    shortened = textwrap.shorten(original, 100)
-    shortened_wrapped = textwrap.fill(shortened, width=50)
+    s = match.start()
+    e = match.end()
 
-    print('Shortened:\n')
-    print(shortened_wrapped)
+    print('Found "{}"\nin "{}"\nfrom {} to {} ("{}")'.format(match.re.pattern, match.string, s, e, text[s:e]))
+
 
 if __name__ == "__main__":
     main()
