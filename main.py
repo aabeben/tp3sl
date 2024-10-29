02448b71b26e2f686c2a7d646b4db71129535da6
Beny Hasanudin <bhasanudin@gmail.com>
2024-10-29 16:33:05 +0700

    Pattern syntax.

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 #!/bin/env python
 import re
 
+def test_patterns(text, patterns):
+    """Given source text and a list of patterns, look for
+    matches for each pattern within the text and print
+    them to stdout.
+    """
+    # Look for each pattern in the text and print the results.
+    for pattern, desc in patterns:
+        print("'{}' ({})\n".format(pattern, desc))
+        print(" '{}'".format(text))
+        for match in re.finditer(pattern, text):
+            s = match.start()
+            e = match.end()
+            substr = text[s:e]
+            n_backslashes = text[:s].count('\\')
+            prefix = '.' * (s + n_backslashes)
+            print(" {}'{}'".format(prefix, substr))
+        print()
+    return
 def main():
-    text = 'abbaaabbbbaaaaa'
-    pattern = 'ab'
-
-    for match in re.findall(pattern, text):
-        print('Found {!r}'.format(match))
-
+    test_patterns('abbaaabbbbaaaaa', [('ab', "'a' followed by 'b'"),])
 if __name__ == "__main__":
     main()
