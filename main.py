35c1d2716f5ed9183bb2eb311970593badd30984
Beny Hasanudin <bhasanudin@gmail.com>
2024-10-29 08:49:17 +0700

    Multiple matches.

--- main.py
+++ main.py
@@ -2,22 +2,11 @@
 import re
 
 def main():
-    # Precompile the pattern.
-    regexes = [
-        re.compile(p)
-        for p in ['this', 'that']
-    ]
-    print(regexes)
-    text = 'Does this text match the pattern?'
-    print('Text: {!r}\n'.format(text))
+    text = 'abbaaabbbbaaaaa'
+    pattern = 'ab'
 
-    for regex in regexes:
-        print('Seeking "{}" ->'.format(regex.pattern), end=' ')
-
-        if regex.search(text):
-            print('match')
-        else:
-            print('no match')
+    for match in re.findall(pattern, text):
+        print('Found {!r}'.format(match))
 
 if __name__ == "__main__":
     main()
