48747af8adb23dac4ec205afbe2f0aec74457181
Beny Hasanudin <bhasanudin@gmail.com>
2024-10-30 13:48:04 +0700

    Constraining the search.

--- main.py
+++ main.py
@@ -1,37 +1,17 @@
 #!/bin/env python
 import re
 
-def test_patterns(text, patterns):
-    """Given source text and a list of patterns, look for
-    matches for each pattern within the text and print
-    them to stdout.
-    """
-    # Look for each pattern in the text and print the results.
-    for pattern, desc in patterns:
-        print("'{}' ({})\n".format(pattern, desc))
-        print(" '{}'".format(text))
-        for match in re.finditer(pattern, text):
-            s = match.start()
-            e = match.end()
-            substr = text[s:e]
-            n_backslashes = text[:s].count('\\')
-            prefix = '.' * (s + n_backslashes)
-            print(" {}'{}'".format(prefix, substr))
-        print()
-    return
 def main():
-    test_patterns(
-        'This is some text -- with punctuation.',
-        [
-            (r'^\w+', 'word at start of string'),
-            (r'\A\w+', 'word at start of string'),
-            (r'\w+\S*$', 'word near end of string'),
-            (r'\w+\S*\Z', 'word near end of string'),
-            (r'\w*t\w*','word containing t'),
-            (r'\bt\w+','t at start of word'),
-            (r'\w+t\b','t at end of word'),
-            (r'\Bt\B','t, not start or end of word'),
-        ]
-    )
+    text = 'This is some text -- with punctuation.'
+    pattern = 'is'
+
+    print('Text :', text)
+    print('Pattern :', pattern)
+
+    m = re.match(pattern, text)
+    print('Match :', m)
+    s = re.search(pattern, text)
+    print('Search :', s)
+
 if __name__ == "__main__":
     main()
