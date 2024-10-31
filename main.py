c70f621fd4e9cd24d8b00eaba7828ec159f7c328
Beny Hasanudin <bhasanudin@gmail.com>
2024-10-31 18:44:36 +0700

    Full match in regex.

--- main.py
+++ main.py
@@ -2,16 +2,16 @@
 import re
 
 def main():
-    text = 'This is some text -- with punctuation.'
+    text = 'This some text -- with punctuation.'
     pattern = 'is'
 
-    print('Text :', text)
-    print('Pattern :', pattern)
+    print('Text: ', text)
+    print('Pattern: ', pattern)
 
-    m = re.match(pattern, text)
-    print('Match :', m)
-    s = re.search(pattern, text)
-    print('Search :', s)
+    m = re.search(pattern, text)
+    print('Search: ', m)
+    s = re.fullmatch(pattern, text)
+    print('Full match: ', s)
 
 if __name__ == "__main__":
     main()
